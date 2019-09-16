f82ba27feeab6ff6944ec18ba58c752870e4d8f7
shubhamvm <51687268+shubhamvm@users.noreply.github.com>
2019-09-16 10:26:26 +0530

    Create Nested Lists.py

---
+++ Python/Basic Data types/Nested Lists.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    l = list()
+    p=set()
+    for _ in range(int(input())):
+        _ = dict()
+        
+        name = input()
+        score = float(input())
+        _[score]=name
+        p.add(score)
+        l.append(_)
