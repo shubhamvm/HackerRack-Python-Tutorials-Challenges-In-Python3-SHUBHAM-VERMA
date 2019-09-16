ba7d8b95bd1c9160f875be3643312353123365ae
shubhamvm <51687268+shubhamvm@users.noreply.github.com>
2019-09-16 10:26:58 +0530

    Create Finding the percentage.py

---
+++ Python/Basic Data types/Finding the percentage.py
@@ -0,0 +1,17 @@
+from decimal import Decimal
+if __name__ == '__main__':
+    n = int(input())
+    a=dict()    
+    student_marks = {}
+    if(n>1):
+        for _ in range(n):
+            c=0
+            name, *line = input().split()
+            scores = list(map(float, line))            
+            student_marks[name] = scores
+            for i in scores:
+                c +=i                       
+            av = c/3            
+            a[name]=av            
+    query_name = input()
+print("{:.2f}".format(a[query_name]))
