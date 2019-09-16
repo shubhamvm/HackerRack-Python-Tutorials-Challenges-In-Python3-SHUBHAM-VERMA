0f3152821f6e1d13daba1e35c52787a51ba172cd
shubhamvm <51687268+shubhamvm@users.noreply.github.com>
2019-09-16 10:36:44 +0530

    Update String Split and Join.py

--- Python/Strings/String Split and Join.py
+++ Python/Strings/String Split and Join.py
@@ -3,3 +3,8 @@ def split_and_join(line):
     result = line.split(" ")
     result = "-".join(result)
     return(result)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
