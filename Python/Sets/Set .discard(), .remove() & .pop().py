2f74040636fe09c8cc8193bcf0568ab7bdae6dcc
shubhamvm <51687268+shubhamvm@users.noreply.github.com>
2019-09-16 11:14:56 +0530

    Update Set .discard(), .remove() & .pop().py

--- Python/Sets/Set .discard(), .remove() & .pop().py
+++ Python/Sets/Set .discard(), .remove() & .pop().py
@@ -1,1 +1,11 @@
-jj
+n = int(input())
+s = set(map(int, input().split()))
+n1 = int(input())
+for i in range(n1):
+    cmd = list(input().split(' '))
+    if (len(cmd) == 1):
+        s.pop()
+    else:
+        value = int(cmd[1])
+        s.discard(value)
+print(sum(s))
