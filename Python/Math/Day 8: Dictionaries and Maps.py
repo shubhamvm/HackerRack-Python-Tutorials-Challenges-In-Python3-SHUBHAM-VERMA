4c8e7b5c643d49dd4408081dd9e877ed81309482
shubhamvm <51687268+shubhamvm@users.noreply.github.com>
2019-09-17 11:11:39 +0530

    Delete Day 8: Dictionaries and Maps.py

--- Python/Math/Day 8: Dictionaries and Maps.py
+++
@@ -1,8 +0,0 @@
-d = dict()
-n = int(input())
-for _ in range(n):
-    a,b = input().split()
-    d[a]=b
-for _ in range(n):
-    i = str(input())
-    print(i+"="+d[i]) if (i in d) else print("Not found")
